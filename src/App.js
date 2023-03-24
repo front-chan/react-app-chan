1198e7b33f76edf27cc8f91c8ba17295ec835516
front-chan <chan.jor.zz@gmail.com>
2023-03-24 17:15:12 +0900

    study components and props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+function Header(props) {
+    console.log(props.title);
+    return (
+        <header>
+            <h1>
+                <a href="/">{props.title}</a>
+            </h1>
+        </header>
+    );
+}
+
+function Nav(props) {
+    const lis = [
+        // <li>
+        //     <a href="/read/1">html</a>
+        // </li>,
+        // <li>
+        //     <a href="/read/2">css</a>
+        // </li>,
+        // <li>
+        //     <a href="/read/3">js</a>
+        // </li>,
+    ];
+    for (let i = 0; i < props.topics.length; i++) {
+        let t = props.topics[i];
+        lis.push(
+            <li key={t.id}>
+                <a href={`/read/${t.id}`}>{t.title}</a>
+            </li>
+        );
+    }
+    return (
+        <nav>
+            <ol>{lis}</ol>
+        </nav>
+    );
+}
+
+function Article(props) {
+    return (
+        <article>
+            <h2>{props.title}</h2>
+            {props.body}
+        </article>
+    );
+}
+
 function App() {
-    return <div className="App">Hello React!</div>;
+    const topics = [
+        { id: 1, title: "html", body: "html is ..." },
+        { id: 2, title: "css", body: "css is ..." },
+        { id: 3, title: "javascript", body: "javascript is ..." },
+    ];
+    return (
+        <div className="App">
+            <Header title="REACT"></Header>
+            <Nav topics={topics}></Nav>
+            <Article title="Welcome" body="Hello, WEB"></Article>
+            <Article title="Hi" body="Hello, React"></Article>
+        </div>
+    );
 }
 
 export default App;
