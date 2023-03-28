0a06d501f67d38b00a75b6c6df5bb624ec7b4557
front-chan <chan.jor.zz@gmail.com>
2023-03-28 16:47:35 +0900

    study state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function Header(props) {
     console.log(props.title);
@@ -41,7 +42,7 @@ function Nav(props) {
                     href={`/read/${t.id}`}
                     onClick={(e) => {
                         e.preventDefault();
-                        props.onChangeMode(e.target.id);
+                        props.onChangeMode(Number(e.target.id));
                         // console.dir(e);
                         // props.onChangeMode(t.id); // 동일하게 동작함
                     }}
@@ -68,27 +69,60 @@ function Article(props) {
 }
 
 function App() {
+    const [mode, setMode] = useState("WELCOME");
+    /*
+    const _mode = useState('WELCOME');
+    const mode = _mode[0];
+    const setMode = _mode[1];
+    >> const [mode, setMode] = useState("WELCOME"); 위에 세줄이 이 한줄과 같은 문법임
+
+    console.log("_mode", _mode);
+    _mode (2) ['WELCOME', function] 출력
+    */
+
+    const [id, setId] = useState(null);
+
     const topics = [
         { id: 1, title: "html", body: "html is ..." },
         { id: 2, title: "css", body: "css is ..." },
         { id: 3, title: "javascript", body: "javascript is ..." },
     ];
+
+    let content = null;
+
+    if (mode === "WELCOME") {
+        content = <Article title="Welcome" body="Hello, WEB"></Article>;
+    } else if (mode === "READ") {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            // console.log(topics[i].id, id);
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = <Article title={title} body={body}></Article>;
+    }
+
     return (
         <div className="App">
             <Header
                 title="WEB"
                 onChangeMode={() => {
-                    alert("Header");
+                    setMode("WELCOME");
                 }}
             ></Header>
             <Nav
                 topics={topics}
-                onChangeMode={(id) => {
-                    alert(id);
+                onChangeMode={(_id) => {
+                    setMode("READ");
+                    setId(_id);
                 }}
             ></Nav>
-            <Article title="Welcome" body="Hello, WEB"></Article>
-            <Article title="Hi" body="Hello, React"></Article>
+            {content}
+            {/* <Article title="Welcome" body="Hello, WEB"></Article> */}
+            {/* <Article title="Hi" body="Hello, React"></Article> */}
         </div>
     );
 }
