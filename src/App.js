55737cbc1a4c256547950b4519c64359920ef3ed
front-chan <chan.jor.zz@gmail.com>
2023-03-28 17:49:12 +0900

    study update

--- src/App.js
+++ src/App.js
@@ -93,6 +93,49 @@ function Create(props) {
         </article>
     );
 }
+
+function Update(props) {
+    const [title, setTitle] = useState(props.title);
+    const [body, setBody] = useState(props.body);
+    return (
+        <article>
+            <h2>Update</h2>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    const title = e.target.title.value;
+                    const body = e.target.body.value;
+                    props.onUpdate(title, body);
+                }}
+            >
+                <p>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="title"
+                        value={title}
+                        onChange={(e) => {
+                            // console.log(e.target.value);
+                            setTitle(e.target.value);
+                        }}
+                    />
+                </p>
+                <p>
+                    <textarea
+                        name="body"
+                        placeholder="body"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    />
+                </p>
+                <p>
+                    <input type="submit" value="Update" />
+                </p>
+            </form>
+        </article>
+    );
+}
+
 function App() {
     const [mode, setMode] = useState("WELCOME");
     /*
@@ -115,6 +158,7 @@ function App() {
     ]);
 
     let content = null;
+    let contextControl = null;
 
     if (mode === "WELCOME") {
         content = <Article title="Welcome" body="Hello, WEB"></Article>;
@@ -129,6 +173,19 @@ function App() {
             }
         }
         content = <Article title={title} body={body}></Article>;
+        contextControl = (
+            <li>
+                <a
+                    href={`/update/${id}`}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setMode("UPDATE");
+                    }}
+                >
+                    Update
+                </a>
+            </li>
+        );
     } else if (mode === "CREATE") {
         content = (
             <Create
@@ -142,6 +199,35 @@ function App() {
                     setNextId(nextId + 1);
                 }}
             ></Create>
+        );
+    } else if (mode === "UPDATE") {
+        let title,
+            body = null;
+        for (let i = 0; i < topics.length; i++) {
+            // console.log(topics[i].id, id);
+            if (topics[i].id === id) {
+                title = topics[i].title;
+                body = topics[i].body;
+            }
+        }
+        content = (
+            <Update
+                title={title}
+                body={body}
+                onUpdate={(title, body) => {
+                    console.log(title, body);
+                    const newTopics = [...topics];
+                    const updatedTopic = { id, title, body };
+                    for (let i = 0; i < newTopics.length; i++) {
+                        if (newTopics[i].id === id) {
+                            newTopics[i] = updatedTopic;
+                            break;
+                        }
+                    }
+                    setTopics(newTopics);
+                    setMode("READ");
+                }}
+            ></Update>
         );
     }
 
@@ -163,15 +249,21 @@ function App() {
             {content}
             {/* <Article title="Welcome" body="Hello, WEB"></Article> */}
             {/* <Article title="Hi" body="Hello, React"></Article> */}
-            <a
-                href="/create"
-                onClick={(e) => {
-                    e.preventDefault();
-                    setMode("CREATE");
-                }}
-            >
-                Create
-            </a>
+
+            <ul>
+                <li>
+                    <a
+                        href="/create"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            setMode("CREATE");
+                        }}
+                    >
+                        Create
+                    </a>
+                </li>
+                {contextControl}
+            </ul>
         </div>
     );
 }
